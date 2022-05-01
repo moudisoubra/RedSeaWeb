6d39a523e5c355666751c907a9e4d03d09b2216b
Soubra <moudisoubra001@gmail.com>
2022-05-01 18:25:34 +0400

    Added Basic Stuff

---
+++ Assets/Scripts/LoadImages.cs
@@ -0,0 +1,81 @@
+using UnityEngine;
+
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine.Video;
+
+public class LoadImages : MonoBehaviour
+{
+
+    public string filesLocation = @"C:/images";
+    public string videofilesLocation = @"C:/images";
+    public List<Texture2D> images = new List<Texture2D>();
+    public List<string> videos = new List<string>();
+    public bool gotAllVideos;
+    public VideoPlayer player;
+    public List<VideoPlayer> videoPlayers = new List<VideoPlayer>();
+    public OrbitObject ooScript;
+
+    public IEnumerator Start()
+    {
+        yield return StartCoroutine(
+            "LoadAll",
+            Directory.GetFiles(filesLocation, "*.png", SearchOption.AllDirectories)
+        );
+        yield return StartCoroutine(
+            "LoadAllMP4",
+            Directory.GetFiles(videofilesLocation, "*.mp4", SearchOption.AllDirectories)
+            );
+
+        player.url = videos[0];
+        player.Play();
+        gotAllVideos = true;
+    }
+
+    public void Update()
+    {
+        if (gotAllVideos)
+        {
+            //player.url = videos[0];
+            //player.Play();
+            ooScript.videos = videos;
+            ooScript.enabled = true;
+            gotAllVideos = false;
+        }
+    }
+
+    public IEnumerator LoadAll(string[] filePaths)
+    {
+        foreach (string filePath in filePaths)
+        {
+            WWW load = new WWW("file:///" + filePath);
+            yield return load;
+            if (!string.IsNullOrEmpty(load.error))
+            {
+                Debug.LogWarning(filePath + " error");
+            }
+            else
+            {
+                images.Add(load.texture);
+            }
+        }
+    }
+
+    public IEnumerator LoadAllMP4(string[] filePaths)
+    {
+        foreach (string filePath in filePaths)
+        {
+            WWW load = new WWW("file:///" + filePath);
+            yield return load;
+            if (!string.IsNullOrEmpty(load.error))
+            {
+                Debug.LogWarning(filePath + " error");
+            }
+            else
+            {
+                videos.Add(filePath);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/OrbitObject.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OrbitObject : MonoBehaviour
+{
+    public bool orbit;
+    public List<string> videos = new List<string>();
+    public GameObject prefab;
+    public List<GameObject> objects = new List<GameObject>();
+    public float radius;
+    public float time;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnObjects(radius, videos);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (orbit)
+        {
+            StartCoroutine(RotateMe(new Vector3(0,0,1) * 90, time));
+            orbit = false;
+        }
+    }
+
+    public void SpawnObjects(float radius, List<string> videos)
+    {
+        for (int i = 0; i < videos.Count; i++)
+        {
+            float theta = i * 2 * Mathf.PI / videos.Count;
+            float x = Mathf.Sin(theta) * radius;
+            float y = Mathf.Cos(theta) * radius;
+        
+
+            GameObject ob = Instantiate(prefab);
+            ob.transform.parent = transform;
+            ob.transform.position = new Vector3(transform.position.x + x, transform.position.y + y, transform.position.z);
+            objects.Add(ob);
+        }
+    }
+
+    IEnumerator RotateMe(Vector3 byAngles, float inTime)
+    {
+        var fromAngle = transform.rotation;
+        var toAngle = Quaternion.Euler(transform.eulerAngles + byAngles);
+        for (var t = 0f; t < 1; t += Time.deltaTime / inTime)
+        {
+            transform.rotation = Quaternion.Slerp(fromAngle, toAngle, t);
+            yield return null;
+        }
+    }
+
+    //public void Orbit(GameObject obj, float speed)
+    //{
+    //    Vector3 pointT = new Vector3(5, 0, 0);
+    //    Vector3 axis = new Vector3(0, 0, 1);
+    //    f += Time.deltaTime * speed;
+
+
+    //    obj.transform.RotateAround(transform.position, axis, f);
+
+    //    if (f >= 1)
+    //    {
+    //        orbit = false;
+    //    }
+    //}
+}
